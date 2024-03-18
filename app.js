024ae866dc0dd8dbafb0628c6de199ef142ee515
Jayobz <141148921+Jayobz@users.noreply.github.com>
2024-03-18 20:46:26 +0800

    Update app.js

--- app.js
+++ app.js
@@ -18,14 +18,17 @@ const data = {
     para1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium exercitationem blanditiis magnam sint modi quaeratrepudiandae atque voluptate cum neque quae beatae pariatur tenetur dolore, dolores corporis impedit sed labore!",
     para2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium exercitationem blanditiis magnam sint modi quaeratrepudiandae atque voluptate cum neque quae beatae pariatur tenetur dolore, dolores corporis impedit sed labore!",
     para3: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium exercitationem blanditiis magnam sint modi quaeratrepudiandae atque voluptate cum neque quae beatae pariatur tenetur dolore, dolores corporis impedit sed labore!",
+    btn1: "Click Here",
+    btn2: "Click Here",
+    btn3: "Click Here"
 }
 
 
-const { title1, para, btn, img, proj, para1, para2, para3,img1, img2, img3 } = data
+const { title1, para, btn, img, proj, para1, para2, para3,img1, img2, img3,btn1,btn2,btn3 } = data
 
 wrapper.append(func1(title1, para, btn, img))
 wrapper.append(func2(img))
 wrapper3.append(cardsTitle(proj))
-wrapper2.append(cards(img1, para1))
-wrapper2.append(cards(img2, para2))
-wrapper2.append(cards(img3, para3))
+wrapper2.append(cards(img1, para1,btn1))
+wrapper2.append(cards(img2, para2,btn2))
+wrapper2.append(cards(img3, para3,btn3))
